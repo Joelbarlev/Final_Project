37859830409bab1c99345cb965abbca23f4db9b2
Joel <164216580+Joelbarlev@users.noreply.github.com>
2024-03-21 15:51:03 +0200

    q3.1 silver step fix
    we managed to fix the problem encountered in previews

--- q3.1.sh
+++ q3.1.sh
@@ -10,17 +10,35 @@ if [ "$#" -ne 2 ]; then
 fi
 
 # Check if both inputs are exactly 5 characters long
-if [ "${#word}" -ne 5 ] || [ "${#colors}" -ne 5 ]; then
-    echo "Both word and colors must be exactly 5 characters long."
-    exit 1
-fi
 
 # Catching the inputs into parameter in upper-case
 word=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 colors=$(echo "$2" | tr '[:lower:]' '[:upper:]')
 
+if [[ $colors == *"S"* ]]; then
 
-#hahhahahaahahahahaa
+  # Creating y_letters with all silver letters in word
+  s_letters=""
 
+  # Loop through each character position in 'colors'
+  for ((i = 1; i <= ${#colors}; i++)); do
+    # Check if the character at position 'i' in 'colors' is "S"
+    if [ "${colors:i-1:1}" = "S" ]; then
+        # Append the character at position 'i' in 'word' to 'extracted_letters'
+        s_letters+=${word:i-1:1}
+    fi
+  done
 
+  # Filtering now the word_bank based on the y_letters
+for (( i=0; i<${#s_letters}; i++ )); do
+    # Construct the regular expression pattern to exclude words containing the current character
+    pattern="${s_letters:i:1}"
+    word_bank=$(echo "$word_bank" | grep -vE "[$pattern]")
+done
+
+# Use grep to filter out words containing any of the characters in $X
+
+fi
+
+echo "$word_bank"
 
