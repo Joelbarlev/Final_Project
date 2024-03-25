a132657bb7a5633ae5d9620b2cc94e4352e85981
Joel <164216580+Joelbarlev@users.noreply.github.com>
2024-03-25 11:48:52 +0200

    Added the python file for q2

---
+++ q2/q2.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get():
+  return str("Usage:") #Prints usage information following a GET
+
+@app.route('/', methods=['POST'])
+def post():
+  received_value = str(request.get_data(as_text=True)) #Gets the data from the POST request
+  answer = calculate_answer(received_value)
+  return str(answer) #Returns the data to the user
+
+def calculate_answer(received_value):
+  #write me
+  return
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0')
