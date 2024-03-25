05459ae1da3376f548b1d10a21894785401b7238
Joel <164216580+Joelbarlev@users.noreply.github.com>
2024-03-25 16:08:39 +0200

    Added the queries (q1-3)
    Added  (q1-3), have problems using flask

--- q2.1/b.sh
+++ q2.1/b.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # B1: download python3-flask
-sudo apt-get install python3-flask
+#sudo apt-get install python3-flask
+# Install Python3-Flask using pip3 for mac
+pip3 install flask
--- q2.1/q2.py
+++ q2.1/q2.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, request
 import subprocess
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -14,6 +13,29 @@ def post():
   received_value = str(request.get_data(as_text=True)) #Gets the data from the POST request
   answer = calculate_answer(received_value)
   return str(answer) #Returns the data to the user
+
+#Aggregation by City and Year
+q1 = '''
+    SELECT City, Year, AVG(Price_per_kilogram) AS Average_Price
+FROM tortilla_prices
+GROUP BY City, Year;
+'''
+
+#Temporal Analysis: Identify the trend of tortilla prices over the years.
+q2 = '''
+SELECT Year, AVG(Price_per_kilogram) AS Average_Price
+FROM tortilla_prices
+GROUP BY Year
+ORDER BY Year;
+'''
+
+#Store Type Comparison:Compare the average prices of tortillas between store types.
+q3 = '''
+    SELECT Store_type, AVG(Price_per_kilogram) AS Average_Price
+FROM tortilla_prices
+GROUP BY Store_type;
+
+'''
 
 def calculate_answer(received_value):
   #write me
